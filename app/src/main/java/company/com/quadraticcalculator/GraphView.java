a744d3600808fff11c926993ae1ab38c053e5dd7
JdGXII <jralzaibar@yahoo.com>
2016-04-07 19:49:11 -0400

    Added GraphView class to inherit from view and add all controllers for both programmatic construction and XML inflation

---
+++ app/src/main/java/company/com/quadraticcalculator/GraphView.java
@@ -0,0 +1,27 @@
+package company.com.quadraticcalculator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jralz_000 on 4/7/2016.
+ */
+public class GraphView extends View {
+    
+    public GraphView(Context context) {
+        super(context);
+    }
+
+    public GraphView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GraphView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public GraphView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
