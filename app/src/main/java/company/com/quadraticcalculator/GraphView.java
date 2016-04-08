75fdca38a7db8aaf8bf27e5bd77f8349f39c1e05
JdGXII <jralzaibar@yahoo.com>
2016-04-07 21:02:51 -0400

    overrid onDraw and onMeasure methods

--- app/src/main/java/company/com/quadraticcalculator/GraphView.java
+++ app/src/main/java/company/com/quadraticcalculator/GraphView.java
@@ -1,6 +1,7 @@
 package company.com.quadraticcalculator;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -8,7 +9,7 @@ import android.view.View;
  * Created by jralz_000 on 4/7/2016.
  */
 public class GraphView extends View {
-    
+
     public GraphView(Context context) {
         super(context);
     }
@@ -23,5 +24,82 @@ public class GraphView extends View {
 
     public GraphView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    /**
+     * Implement this to do your drawing.
+     *
+     * @param canvas the canvas on which the background will be drawn
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    /**
+     * <p>
+     * Measure the view and its content to determine the measured width and the
+     * measured height. This method is invoked by {@link #measure(int, int)} and
+     * should be overridden by subclasses to provide accurate and efficient
+     * measurement of their contents.
+     * </p>
+     * <p/>
+     * <p>
+     * <strong>CONTRACT:</strong> When overriding this method, you
+     * <em>must</em> call {@link #setMeasuredDimension(int, int)} to store the
+     * measured width and height of this view. Failure to do so will trigger an
+     * <code>IllegalStateException</code>, thrown by
+     * {@link #measure(int, int)}. Calling the superclass'
+     * {@link #onMeasure(int, int)} is a valid use.
+     * </p>
+     * <p/>
+     * <p>
+     * The base class implementation of measure defaults to the background size,
+     * unless a larger size is allowed by the MeasureSpec. Subclasses should
+     * override {@link #onMeasure(int, int)} to provide better measurements of
+     * their content.
+     * </p>
+     * <p/>
+     * <p>
+     * If this method is overridden, it is the subclass's responsibility to make
+     * sure the measured height and width are at least the view's minimum height
+     * and width ({@link #getSuggestedMinimumHeight()} and
+     * {@link #getSuggestedMinimumWidth()}).
+     * </p>
+     *
+     * @param widthMeasureSpec  horizontal space requirements as imposed by the parent.
+     *                          The requirements are encoded with
+     *                          {@link MeasureSpec}.
+     * @param heightMeasureSpec vertical space requirements as imposed by the parent.
+     *                          The requirements are encoded with
+     *                          {@link MeasureSpec}.
+     * @see #getMeasuredWidth()
+     * @see #getMeasuredHeight()
+     * @see #setMeasuredDimension(int, int)
+     * @see #getSuggestedMinimumHeight()
+     * @see #getSuggestedMinimumWidth()
+     * @see MeasureSpec#getMode(int)
+     * @see MeasureSpec#getSize(int)
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredHeight = measureHeight(heightMeasureSpec);
+        int measuredWidth = measureWidth(widthMeasureSpec);
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+    //Discounts the left and right padding to the width
+    private int measureWidth(int widthMeasureSpec)
+    {
+        int specSize = MeasureSpec.getSize(widthMeasureSpec) -
+                this.getPaddingLeft() - this.getPaddingRight();
+        return specSize;
+    }
+    private int measureHeight(int heightMeasureSpec)
+    {
+        int specSize = MeasureSpec.getSize(heightMeasureSpec);
+        return specSize;
     }
 }
